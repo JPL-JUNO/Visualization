a0fdaa1fa4dbf185381877255a651aa607c0f4d7
Stephen CUI <junocuixuan@163.com>
2024-01-22 22:23:43 +0800

    颜色使用

--- others/9787121348884/code/dradf.py
+++ others/9787121348884/code/dradf.py
@@ -18,28 +18,8 @@ from matplotlib.ticker import AutoMinorLocator, MultipleLocator, FuncFormatter
 plt.rcParams["font.sans-serif"] = ["SimHei"]
 plt.rcParams["axes.unicode_minus"] = False
 
-font_style = dict(color="saddlebrown", weight="semibold", size=16)
-line_marker_style = dict(
-    linestyle=":",
-    linewidth=2,
-    color="cornflowerblue",
-    markerfacecoloralt="lightgrey",
-    marker="o",
-    markersize=18,
-)
-fig = plt.figure()
-ax = fig.add_subplot(111)
-fill_style_list = ["full", "left", "right", "bottom", "top", "none"]
-x = np.arange(3, 11, 1)
-y = np.linspace(1, 1, 8)
-for i, fs in enumerate(fill_style_list):
-    ax.text(0, i + 0.4, "'{}'".format(fs), **font_style)
-    ax.plot(x, (i + 0.5) * y, fillstyle=fs, **line_marker_style)
-ax.set_xlim(-1, 11)
-ax.set_ylim(0, 6)
-ax.margins(0.3)
-# ax.set_xticks([])
-# ax.set_yticks([])
-ax.axis("off")
-plt.title("标记填充样式的设置方法")
+rd = np.random.rand(10, 10)
+plt.pcolor(rd, cmap="BuPu")
+plt.colorbar()
+plt.title("模拟图的颜色使用模式")
 plt.show()
